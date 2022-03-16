7fed9d92d15644f7432c90d1b4348a640e91e8ad
marcelo-gomj <marcelomgj005@gmail.com>
2022-03-16 15:37:13 -0300

    add header and form in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl} from '@chakra-ui/react';
+import { Flex, Button, Stack} from '@chakra-ui/react';
+import { Input } from '../components/Form';
 
 export default function Home() {
     return (
@@ -18,37 +19,17 @@ export default function Home() {
                     flexDir="column"
                 >
                     <Stack spacing="4">
-                        <FormControl>
-                            <FormLabel htmlFor='email'>Email</FormLabel>
-                            <Input 
-                                name="email" 
-                                type="email" 
-                                id='email'
-                                focusBorderColor="pink.500"
-                                bgColor="gray.900"
-                                variant="filled"
-                                _hover={{
-                                    bgColor : 'gray.900'
-                                }}
-                                size="lg"
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor='password'>Senha</FormLabel>
-                            <Input 
-                                name="password" 
-                                type="password" 
-                                id="password"
-                                focusBorderColor="pink.500"
-                                bgColor="gray.900"
-                                variant="filled"
-                                _hover={{
-                                    bgColor : 'gray.900'
-                                }}
-                                size="lg"
+                        <Input 
+                            name="email" 
+                            type="email"
+                            label="E-mail" 
+                        />
 
-                            />
-                        </FormControl>
+                        <Input 
+                            name='password'
+                            label='password'
+                            type='Senha'
+                        />
                     </Stack>
 
                     <Button 
