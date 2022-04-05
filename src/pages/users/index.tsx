72ab8c2a3afecb4e74b2846ff43229150aed7e8c
marcelo-gomj <marcelomgj005@gmail.com>
2022-04-05 16:00:11 -0300

    add: axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,11 +7,13 @@ import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { useState } from 'react';
 
 export default function UserList(){
-    const {data, isLoading, error } = useQuery("users", async () => {
-        const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json();
+    const [page, setPage] = useState();
+    const {data, isLoading, isFetching, error } = useQuery("users", async () => {
+        const { data } = await api.get('/users');
 
         const users = data.users.map(user => {
             return {
@@ -66,6 +68,7 @@ export default function UserList(){
                             fontWeight="normal"
                         >
                             Úsuarios
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
                         </Heading>
 
                         <Link href="/users/create" passHref>
